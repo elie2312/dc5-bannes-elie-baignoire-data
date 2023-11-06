df3e46e883f17986b4575c94a669aa68346d8144
elie2312 <elie.bannes12@gmail.com>
2023-11-06 08:40:41 +0100

    Ajout exo1 a

---
+++ ex1-a.py
@@ -0,0 +1,10 @@
+nom_entreprise = "Société"
+nb_clients = 10
+revenu_moyen_par_client = 100
+
+revenu_total = nb_clients * revenu_moyen_par_client
+
+print("Nom de l'entreprise :", nom_entreprise)
+print("Nombre de clients :", nb_clients)
+print("Revenu moyen par client :", revenu_moyen_par_client)
+print("Revenu total :", revenu_total)
