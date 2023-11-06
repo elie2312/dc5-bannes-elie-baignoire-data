ca8df20a1c3f155c8ee7a9ed1d1b09dcd272d634
elie2312 <elie.bannes12@gmail.com>
2023-11-06 08:45:45 +0100

    ajout exo2 a

---
+++ ex2-a.py
@@ -0,0 +1,7 @@
+depenses_mensuelles = [100, 150, 100, 150, 100, 50, 300, 200, 20, 100, 1000, 500]
+
+depenses_annuelles = 0
+for depense in depenses_mensuelles:
+    depenses_annuelles += depense
+
+print("Dépenses totales de l'année :", depenses_annuelles)
