7a9faf2d85aa689538b3c3e5651ab4b38dfc6fe6
elie2312 <elie.bannes12@gmail.com>
2023-11-06 08:53:54 +0100

    ajout exo3

---
+++ ex3.py
@@ -0,0 +1,10 @@
+cout_campagne = 5000
+
+revenu_campagne = 10000
+
+if revenu_campagne > cout_campagne:
+    print("La campagne a été rentable.")
+elif revenu_campagne == cout_campagne:
+    print("La campagne a été à l'équilibre.")
+else:
+    print("La campagne n'a pas été rentable.")
